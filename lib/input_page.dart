c262fa3a9ac04797920845f3761e88048c32b88e
avajiyo <ava@avacado.page>
2020-09-25 20:09:01 +1000

    Implemented gender selection (card colour changing on click) using GestureDetector and setState (Created a new enum 'Gender')

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'reusable_card.dart';
 import 'card_child.dart';
@@ -9,6 +10,8 @@ const headingStyle = TextStyle(
   fontWeight: FontWeight.normal,
   fontSize: 20,
 );
+const normalCardColour = Color(0xFF101015);
+const pressedCardColour = Colors.amber;
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -19,7 +22,31 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+enum Gender { male, female }
+
 class _MyHomePageState extends State<MyHomePage> {
+  Color maleCardColour = normalCardColour;
+  Color femaleCardColour = normalCardColour;
+
+  //Change the card colour when gender is selected.
+  void selectGender(Gender gender) {
+    if (gender == Gender.male) {
+      if (maleCardColour == pressedCardColour) {
+        maleCardColour = normalCardColour;
+      } else {
+        femaleCardColour = normalCardColour;
+        maleCardColour = pressedCardColour;
+      }
+    } else {
+      if (femaleCardColour == pressedCardColour) {
+        femaleCardColour = normalCardColour;
+      } else {
+        maleCardColour = normalCardColour;
+        femaleCardColour = pressedCardColour;
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,15 +64,30 @@ class _MyHomePageState extends State<MyHomePage> {
               child: Row(
                 children: [
                   Expanded(
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          selectGender(Gender.male);
+                        });
+                      },
                       child: ReusableCard(
-                          colour: cardColor,
+                          colour: maleCardColour,
                           cardChild: CardChild(
-                              icon: FontAwesomeIcons.male, text: 'MALE'))),
+                              icon: FontAwesomeIcons.male, text: 'MALE')),
+                    ),
+                  ),
                   Expanded(
-                      child: ReusableCard(
-                          colour: cardColor,
-                          cardChild: CardChild(
-                              icon: FontAwesomeIcons.female, text: 'FEMALE'))),
+                      child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectGender(Gender.female);
+                      });
+                    },
+                    child: ReusableCard(
+                        colour: femaleCardColour,
+                        cardChild: CardChild(
+                            icon: FontAwesomeIcons.female, text: 'FEMALE')),
+                  )),
                 ],
               ),
             ),
